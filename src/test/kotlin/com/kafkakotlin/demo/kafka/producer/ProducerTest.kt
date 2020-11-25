dae174da801630abfcad4a7a299b397f0b1d865f
Stephen Roberts <stephen.roberts.skybettingandgaming.com>
2020-11-25 16:01:37 +0000

    WIP

--- src/test/kotlin/com/kafkakotlin/demo/kafka/producer/ProducerTest.kt
+++ src/test/kotlin/com/kafkakotlin/demo/kafka/producer/ProducerTest.kt
@@ -1,6 +1,5 @@
 package com.kafkakotlin.demo.kafka.producer
 
-// import com.kafkakotlin.demo.kafka.KafkaConfiguration
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
